e7f595cc3b5e0bcc0da2965b0d2a1b97f9cee16f
Mayur <mayur9210@gmail.com>
2022-02-28 12:36:32 -0500

    test

--- src/main/java/com/passwordmanager/data/vector/Sample.java
+++ src/main/java/com/passwordmanager/data/vector/Sample.java
@@ -1,8 +1,14 @@
 package com.passwordmanager.data.vector;
 
 public class Sample {
+
+    private String name;
+
+    public Sample(String name) {
+        this.name = name;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Test Vector");
     }
 
 }
