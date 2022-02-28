e47ad1c62cf9a26da9884f22d5e088136fab0e26
Mayur <mayur9210@gmail.com>
2022-02-28 12:37:33 -0500

    test

--- src/main/java/com/passwordmanager/data/vector/Sample.java
+++ src/main/java/com/passwordmanager/data/vector/Sample.java
@@ -8,7 +8,12 @@ public class Sample {
         this.name = name;
     }
 
-    public static void main(String[] args) {
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
