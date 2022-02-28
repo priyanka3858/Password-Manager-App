bd2ae293e0e72923d537374806f3d8257fb0d691
Mayur <mayur9210@gmail.com>
2022-02-28 15:38:52 -0500

    generics

--- src/main/java/com/passwordmanager/data/vector/Sample.java
+++ src/main/java/com/passwordmanager/data/vector/Sample.java
@@ -1,23 +1,38 @@
-package com.passwordmanager.data.vector;
 
-public class Sample {
 
-    private String name;
 
-    public Sample(String name) {
-        this.name = name;
+// Generic class example to show how to setup generic classes type for base configuration type
+public class BaseConfigurationType<T> {
+    // an object of type T is declared
+    private T type;
+
+    // constructor
+    public BaseConfigurationType(T type){
+        this.type = type;
     }
 
-    public String getName() {
-        return name;
+    public BaseConfigurationType() {
+
     }
 
-    public void setName(String name) {
-        this.name = name;
+    // getter method
+    public T getObject() {
+        return type;
     }
-
-    public static int parseInt(String name) {
-        return Integer.parseInt(name);
+    // setter method
+    public void setObject(T type) {
+        this.type = type;
     }
 }
 
+
+// Use of generic class
+public void setDarkMode(){;
+    BaseConfigurationType<Boolean> darkMode = new BaseConfigurationType<>(false);
+
+    darkMode.setObject(true);
+
+    if (darkMode.getObject()) {
+        // do something
+    }
+}
